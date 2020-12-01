6e3510a7925dc6231d1f770fe25cc633afd63678
Mark Woo <mwwoon@amazon.com>
2020-12-01 13:14:12 -0800

    redo build.sbt

--- 2020/src/main/scala/Main.scala
+++ 2020/src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
 object Main extends App {
-  println("Hello, World!")
+  val fileStream = getClass.getResourceAsStream("day1.txt")
+  val lines = Source.fromInputStream(fileStream).getLines
+  val input = lines
+  for (line <- input) {
+    println(line)
+  }
 }
